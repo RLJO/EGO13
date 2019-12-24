bb314ef5e6481aeea843c45878c597696fd83063
ibrahim.m <ibrahimmohamedd190@gmail.com>
2019-12-24 15:50:49 +0200

    update purchase order

--- ego_sale_purchase_custom/models/purchase_order_custom.py
+++ ego_sale_purchase_custom/models/purchase_order_custom.py
@@ -4,11 +4,11 @@ from odoo.exceptions import UserError, ValidationError
 class PURCHASEORDER(models.Model):
     _inherit = 'purchase.order'
     approve_check=fields.Boolean()
-    partner_id = fields.Many2one('res.partner', string='Vendor',
+    partner_idd = fields.Many2one('res.partner', string='Vendor',
                                  change_default=True, tracking=True,required=False,
                                  domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
                                  help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
-    company_id = fields.Many2one('res.company', 'Company', required=False,index=True, default=lambda self: self.env.company.id)
+    company_idd = fields.Many2one('res.company', 'Company', required=False,index=True, default=lambda self: self.env.company.id)
 
 
 
